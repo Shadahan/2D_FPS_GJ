bb747f1eb17a65906a41532c0fc4833f08c96025
Shada <57822055+Shadahan@users.noreply.github.com>
2021-11-25 06:07:26 +0100

    MainMenu, 2 demo lvls & lvl progression
    Created 2 simple levels for the player to run around, kill enemies and collect pickups.
    Created a simple main menu with Play and Exit buttons.
    Created a transition between lvls, and in case there are no more lvls, quit the game.

---
+++ 2D_FPS/Assets/Scripts/Menu/NextLvl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextLvl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player")
+        {
+            if(SceneManager.GetActiveScene().name == "Test"){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            } else 
+            {
+                Application.Quit();
+            } 
+        } 
+    }
+}
